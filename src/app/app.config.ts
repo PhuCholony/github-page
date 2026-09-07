bef220d32a0aa238ab8a25272385ed07c0e8aff3
Phuchon Chueapong <phucholony@gmail.com>
2026-09-07 21:56:26 +0700

    add angular routing feature
    https://angular.dev/guide/routing

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,10 +4,12 @@ import {
 } from '@angular/core'
 import { provideHttpClient, withInterceptors } from '@angular/common/http'
 import { provideServiceWorker } from '@angular/service-worker'
+import { provideRouter } from '@angular/router'
 
 import { urlInterceptor } from './interceptors/url.interceptor'
 
 import { environment as env } from '../env/environment.production'
+import { routes } from './app.routes'
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -17,5 +19,6 @@ export const appConfig: ApplicationConfig = {
       enabled: env.ENABLE_PWA,
       registrationStrategy: 'registerWhenStable:30000',
     }),
+    provideRouter(routes),
   ],
 }
---
+++ src/app/app.routes.ts
@@ -0,0 +1,14 @@
+import { Routes } from '@angular/router'
+
+import * as General from './pages/general'
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: General.HomePageComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+]
