582b99a315df690524265ce08cc46c974429c844
Phuchon Chueapong <phucholony@gmail.com>
2026-09-07 22:23:16 +0700

    create static module
    Create Header Component into Static Module
    Create Navbar Component into Static Module
    Create Footer Component into Static Module
    Import Static Module and Add 3 above components to App Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core'
 import { RouterOutlet } from '@angular/router'
 
+import { StaticModule } from './modules/static/static.module'
+
 @Component({
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, StaticModule],
   selector: 'app-root',
   styleUrl: './app.component.css',
   templateUrl: './app.component.html',
