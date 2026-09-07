bdfc7aa29c255eac4bf02c41901caa09a030c46f
Phuchon Chueapong <phucholony@gmail.com>
2026-09-07 17:20:49 +0700

    clear app template component
    Clear app html template
    Remove should render title unit testing
    Remove signal title property in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core'
+import { Component } from '@angular/core'
 
 @Component({
   imports: [],
@@ -6,6 +6,4 @@ import { Component, signal } from '@angular/core'
   styleUrl: './app.component.css',
   templateUrl: './app.component.html',
 })
-export class AppComponent {
-  protected readonly title = signal('PhuCholony')
-}
+export class AppComponent {}
