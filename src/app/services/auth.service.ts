deebc272c0cbf843a9c143c2debbaca7f80a86c9
Phuchon Chueapong <phucholony@gmail.com>
2026-09-09 14:35:46 +0700

    remove check authenticate from login auth service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpErrorResponse } from '@angular/common/http'
+import { HttpClient } from '@angular/common/http'
 import { Service, signal, inject } from '@angular/core'
 
 export interface User {
@@ -28,31 +28,6 @@ export class AuthService {
     window.open(
       `https://itch.io/user/oauth?client_id=20d6d200a84c78b3bcd91f33af78691e&scope=profile%3Ame&response_type=token&redirect_uri=http%3A%2F%2Flocalhost%3A4200%2Foauth.html&state=${csrfToken}`,
       '_blank',
-    )
-
-    const controller = new AbortController()
-    document.addEventListener(
-      'visibilitychange',
-      () => {
-        if (document.visibilityState === 'visible') {
-          this.http
-            .get<{
-              accessToken: string
-              csrfToken: string
-            }>('/auth/grant_token', { credentials: 'include' })
-            .subscribe({
-              next: (tokens) => {
-                // TODO: Verify CSRF Token and Save Access Token to Memory
-                console.info(tokens)
-
-                // Login successfully
-                controller.abort()
-              },
-              error: (err: HttpErrorResponse) => console.error(err),
-            })
-        }
-      },
-      { signal: controller.signal },
     )
   }
 }
