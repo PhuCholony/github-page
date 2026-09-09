4d56e28f48405ce5414fe68b4cfc79053555c036
Phuchon Chueapong <phucholony@gmail.com>
2026-09-09 16:44:56 +0700

    remove login method from auth service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -16,18 +16,4 @@ export class AuthService {
   readonly auth = signal<boolean>(false)
   readonly user = signal<User | null>(null)
   readonly role = signal<Role>(Role.Anonymous)
-
-  login() {
-    let csrfToken = window.sessionStorage.getItem('csrf-token')
-    if (!window.sessionStorage.getItem('csrf-token')) {
-      csrfToken = crypto.randomUUID()
-      window.sessionStorage.setItem('csrf-token', csrfToken)
-    }
-
-    // Open oauth new tab from itch.io provider
-    window.open(
-      `https://itch.io/user/oauth?client_id=20d6d200a84c78b3bcd91f33af78691e&scope=profile%3Ame&response_type=token&redirect_uri=http%3A%2F%2Flocalhost%3A4200%2Foauth.html&state=${csrfToken}`,
-      '_blank',
-    )
-  }
 }
