af20feeecde5259a54c9f225d982b85bae2e322c
Phuchon Chueapong <phucholony@gmail.com>
2026-09-12 08:08:17 +0700

    add logout method into auth service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -20,9 +20,15 @@ export class AuthService {
   readonly user = signal<User | null>(null)
   readonly role = signal<Role>(Role.Anonymous)
 
-  login(user: User, role: Role) {
+  login(user: User, role: Role): void {
     this.auth.set(true)
     this.user.set(user)
     this.role.set(role)
+  }
+
+  logout(): void {
+    this.auth.set(false)
+    this.user.set(null)
+    this.role.set(Role.Anonymous)
   }
 }
